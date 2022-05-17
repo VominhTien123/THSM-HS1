ad9dc1015d6e65440ff664f8f63f8286fe2128fe
loctocdo299 <105327285+VominhTien123@users.noreply.github.com>
2022-05-17 08:44:04 +0700

    upload

---
+++ app/src/main/java/com/example/kts1/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.kts1;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void xulyThoat (View view) {
+        finish();
+    }
+
+}
